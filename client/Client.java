6a23f2aba538dd468b8735de1e07cf5175e0f3f2
Сергей Гонтаренко <35502023+sergiy22@users.noreply.github.com>
2020-12-08 22:23:18 +0200

    Add files via upload

---
+++ client/Client.java
@@ -0,0 +1,38 @@
+import java.net.*;
+
+public class MulticastClient {
+    private static String group;
+    private static InetAddress address;
+    private static byte[] buffer;
+    private static DatagramPacket packet;
+    private static String str;
+    private static MulticastSocket socket;
+
+    public static void main(String args[]) throws Exception {
+        group = "192.168.0.1";
+        System.out.println("Очікування повідомлення від сервера");
+        try {
+            socket = new MulticastSocket(1502);
+            address = InetAddress.getByName(group);
+            socket.joinGroup(address);
+            while (true) {
+                buffer = new byte[1024];
+                packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                str = new String(packet.getData());
+                System.out.println("Отримано повідомлення: " + str.trim());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.leaveGroup(address);
+                socket.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+}
