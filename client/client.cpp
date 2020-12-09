ed1fe3a3e6fb636909280633645dba5214d34c84
Сергей Гонтаренко <35502023+sergiy22@users.noreply.github.com>
2020-12-09 15:17:59 +0200

    lab4

---
+++ client/client.cpp
@@ -0,0 +1,65 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include<stdio.h>
+#include<winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib") 
+
+
+#define SERVER "127.0.0.1"
+#define BUFLEN 512
+#define PORT 8888
+
+int main(void)
+{
+	struct sockaddr_in si_other;
+	int s, slen = sizeof(si_other);
+	char buf[BUFLEN];
+	char message[BUFLEN];
+	WSADATA wsa;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	printf("Initialised.\n");
+
+	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
+	{
+		printf("socket() failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+
+	memset((char*)&si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+	while (1)
+	{
+		printf("Enter message : ");
+		gets_s(message);
+
+		if (sendto(s, message, strlen(message), 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
+		{
+			printf("sendto() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		memset(buf, '\0', BUFLEN);
+
+		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr*)&si_other, &slen) == SOCKET_ERROR)
+		{
+			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		puts(buf);
+	}
+
+	closesocket(s);
+	WSACleanup();
+
+	return 0;
+}
